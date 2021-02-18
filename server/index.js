e6220ab010ab70d56aef8d0ac4700f13d04dd3ce
afei <53498870@qq.com>
2021-02-18 15:35:42 +0800

    解决www访问跨域

--- server/index.js
+++ server/index.js
@@ -5,6 +5,20 @@ const path = require("path")
 //连接数据库
 require("./middleware/mongoose")
 
+//跨域
+app.all('*', (req, res, next) => {
+  let host = (req.headers.referer+"").replace(/\/$/,"");
+  if (host === "http://www.afei.fun" || host === "http://afei.fun"){
+    res.header("Access-Control-Allow-Origin", host);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials","true");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+  }
+  next();
+});
+
 //中间件
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
